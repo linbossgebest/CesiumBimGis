9d18ed5ec2a8c133dd39df087004d57b4612f4b7
linbossgebest <809621397@qq.com>
2021-11-08 14:49:04 +0800

    update rabbitmq

---
+++ Cesium.Core/RabbitMQ/IRabbitMQConnection.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesium.Core.RabbitMQ
+{
+    /// <summary>
+    /// RabbitMQ持久连接 接口
+    /// </summary>
+    public interface IRabbitMQConnection : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreateModel();
+    }
+}
