4a73be7319e94d72af581426ef88ff67896f0213
linbossgebest <809621397@qq.com>
2021-11-24 16:15:46 +0800

    update

--- Cesium.Respository/System/SysUserRepository.cs
+++ Cesium.Respository/System/SysUserRepository.cs
@@ -24,62 +24,60 @@ namespace Cesium.Respository
 
         public async Task<bool> AddOrUpdate(UserModel model, TokenInfo tokenInfo)
         {
-            using (var transaction = _dbConnection.BeginTransaction())
+            using var transaction = _dbConnection.BeginTransaction();
+            try
             {
-                try
+                SysUser user;
+                List<SysUserRole> sysUserRoles = new List<SysUserRole>();
+                if (model.Id == 0)
                 {
-                    SysUser user;
-                    List<SysUserRole> sysUserRoles = new List<SysUserRole>();
-                    if (model.Id == 0)
+                    user = new SysUser
                     {
-                        user = new SysUser
-                        {
-                            UserName = model.UserName,
-                            PassWord = AESEncryptHelper.Encode(model.PassWord.Trim(), CesiumKeys.AesEncryptKeys),
-                            Mobile = model.Mobile,
-                            Email = model.Email,
-                            CreateTime = DateTime.Now,
-                            CreatorId = tokenInfo.UserId,
-                            CreatorName = tokenInfo.UserName,
-                            IsEnabled = 1
-                        };
+                        UserName = model.UserName,
+                        PassWord = AESEncryptHelper.Encode(model.PassWord.Trim(), CesiumKeys.AesEncryptKeys),
+                        Mobile = model.Mobile,
+                        Email = model.Email,
+                        CreateTime = DateTime.Now,
+                        CreatorId = tokenInfo.UserId,
+                        CreatorName = tokenInfo.UserName,
+                        IsEnabled = 1
+                    };
 
-                        user.Id = (int)await _dbConnection.InsertAsync(user, transaction);//新增用户信息
-                    }
-                    else
-                    {
-                        user = await _dbConnection.GetAsync<SysUser>(model.Id);
-                        user.UserName = model.UserName;
-                        user.Mobile = model.Mobile;
-                        user.Email = model.Email;
-                        user.ModifyTime = DateTime.Now;
-                        user.ModifyId = tokenInfo.UserId;
-                        user.ModifyName = tokenInfo.UserName;
-
-                        await _dbConnection.UpdateAsync(user, transaction);//修改用户信息
-                    }
-                    foreach (var item in model.RoleIds)
-                    {
-                        SysUserRole sysUserRole = new()
-                        {
-                            UserId = user.Id,
-                            RoleId = item
-                        };
-                        sysUserRoles.Add(sysUserRole);
-                    }
-                    string sql = @"INSERT INTO SysUserRole (UserId,RoleId) VALUES (@UserId,@RoleId); ";
-
-                    await _dbConnection.DeleteListAsync<SysUserRole>(new { UserId = user.Id }, transaction);//删除该用户角色信息
-                    await _dbConnection.ExecuteAsync(sql, sysUserRoles, transaction);//添加该用户角色信息
-
-                    transaction.Commit();
-                    return true;
+                    user.Id = (int)await _dbConnection.InsertAsync(user, transaction);//新增用户信息
                 }
-                catch (Exception ex)
+                else
                 {
-                    transaction.Rollback();
-                    return false;
+                    user = await _dbConnection.GetAsync<SysUser>(model.Id);
+                    user.UserName = model.UserName;
+                    user.Mobile = model.Mobile;
+                    user.Email = model.Email;
+                    user.ModifyTime = DateTime.Now;
+                    user.ModifyId = tokenInfo.UserId;
+                    user.ModifyName = tokenInfo.UserName;
+
+                    await _dbConnection.UpdateAsync(user, transaction);//修改用户信息
                 }
+                foreach (var item in model.RoleIds)
+                {
+                    SysUserRole sysUserRole = new()
+                    {
+                        UserId = user.Id,
+                        RoleId = item
+                    };
+                    sysUserRoles.Add(sysUserRole);
+                }
+                string sql = @"INSERT INTO SysUserRole (UserId,RoleId) VALUES (@UserId,@RoleId); ";
+
+                await _dbConnection.DeleteListAsync<SysUserRole>(new { UserId = user.Id }, transaction);//删除该用户角色信息
+                await _dbConnection.ExecuteAsync(sql, sysUserRoles, transaction);//添加该用户角色信息
+
+                transaction.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return false;
             }
         }
 
@@ -90,22 +88,20 @@ namespace Cesium.Respository
         /// <returns></returns>
         public async Task<bool> DeleteUserInfo(int userId)
         {
-            using (var transaction = _dbConnection.BeginTransaction())
+            using var transaction = _dbConnection.BeginTransaction();
+            try
             {
-                try
-                {
-                    //删除用户信息
-                    await _dbConnection.DeleteAsync<SysUser>(userId, transaction);
-                    //删除该用户对应的用户-角色表关联信息
-                    await _dbConnection.DeleteListAsync<SysUserRole>(new { UserId = userId }, transaction);
-                    transaction.Commit();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    transaction.Rollback();
-                    return false;
-                }
+                //删除用户信息
+                await _dbConnection.DeleteAsync<SysUser>(userId, transaction);
+                //删除该用户对应的用户-角色表关联信息
+                await _dbConnection.DeleteListAsync<SysUserRole>(new { UserId = userId }, transaction);
+                transaction.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return false;
             }
         }
     }
